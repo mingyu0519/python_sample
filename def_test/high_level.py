8b938e899405353a663c9d686c2c66ec0f4a11ab
Mingyu Wang <wang.mingyu@rytong.com>
2016-07-08 21:13:36 +0800

    commit python high-level

--- def_test/high_level.py
+++ def_test/high_level.py
@@ -3,4 +3,17 @@ def slices(listName):
     newList = listName[2:3]
     for value in newList:
         print value
-slices(['哈士奇','泰迪','藏獒','茶杯犬'])
+# slices(['哈士奇','泰迪','藏獒','茶杯犬'])
+
+def iterationName():
+    list1 = [1,2,3]
+    for value in list1:
+        print value
+    dit1 = {'name':'wangxiaoming','age':18,'city':'Beijing'}
+    for key in dit1:
+        print key
+    for value in dit1.itervalues():
+        print value
+    for k,v in dit1.iteritems():
+        print 'key is {0}, value is {1}'.format(k,v)
+iterationName()
