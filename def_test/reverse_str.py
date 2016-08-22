45edfeb9a077d1773bd7a939623c94e6efe1b81d
xiaomingtx <mingyu0519@gmail.com>
2016-08-22 18:17:26 +0800

    add annotation

--- def_test/reverse_str.py
+++ def_test/reverse_str.py
@@ -1,20 +1,24 @@
 # -*- coding:utf-8 -*-
 
+# 使用for循环递减得出每一位的字母
 def reverse(str):
 	r = ''
 	for x in xrange(len(str)-1, -1 , -1):
 		r = r + str[x]
 	return r
 
+# 先转换成list，再反转list，再使用join方法转换list为字符串
 def reverse2(str):
 	li = list(str)
 	li.reverse()
 	str = "".join(li)
 	return str
 
+# 以反向截取整个字符串
 def reverse3(str):
 	return str[::-1]
 
+# str默认转换为list类型，然后翻转，再转化为字符串
 def reverse4(str):
 	return "".join(reversed(str))
 
