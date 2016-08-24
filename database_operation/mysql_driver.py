19c4652d3bfd47150e43c34da8a8ffeb18cb0d5a
Mingyu Wang <mingyu0519@gmail.com>
2016-08-24 22:35:08 +0800

    add mysql driver test

---
+++ database_operation/mysql_driver.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+import mysql.connector
+
+conn = mysql.connector.connect(user='root',password='',database='test',use_unicode=True)
+
+cursor = conn.cursor()
+
+cursor.execute("select * from myclass")
+
+values = cursor.fetchall()
+
+for x in values:
+	for i in x:
+		print i,
+	print ''
+cursor.close()
+conn.close()
